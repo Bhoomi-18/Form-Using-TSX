2edc8df689762c29c6d034cbc5e47a5f1e393cea
Bhoomi-18 <abhoomi20@gmail.com>
2025-07-22 13:08:08 +0530

    hosting

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,10 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN,
+  origin: "https://user-registration-form-sooty.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
+app.options("*", cors());
 
 app.use("/api", userRoutes);
 
