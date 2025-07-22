34128f5bcfa561b0be4a231883340061f17b86d5
Bhoomi-18 <abhoomi20@gmail.com>
2025-07-22 13:21:48 +0530

    hosting

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,8 +15,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://user-registration-form-sooty.vercel.app'
+];
 app.use(cors({
-  origin: "https://user-registration-form-sooty.vercel.app",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
